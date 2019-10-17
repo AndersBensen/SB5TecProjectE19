0a861f3c40257339ea00cb400f6d0c71e29d3138
Erik <erik@hagebjerg.dk>
2019-10-17 12:28:56 +0200

    button working

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
 let wsServer = require('./wsServer.js');
 
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const app = express();
 const port = 8000;
 
-/* app.use(cors({
-    origin: '*'
-})); */
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/server.html'));
-
 });
 
 app.get('/new-server', (req, res) => {
-    /* res.writeHead(200, {'Access-Control-Allow-Origin': '*'}); */
     wsServer.newServer();
+    res.redirect("/");
 });
 
 app.listen(port, () => {
