49472caa6730fe9e7042f0e37c6e967e3684cd76
Erik <erik@hagebjerg.dk>
2019-10-17 11:17:34 +0200

    added express server

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-/* const connect = require('connect');
-
-let serverStatic = require('serve-static');
-
-connect().use(serverStatic(__dirname)).listen(8000, () => {
-    console.log('Server running on 8000...');
-});
-
-console.log('loaded');
-
-
- */
-
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '../server.html'));
+    res.sendFile(path.join(__dirname + '/server.html'));
 });
-
-
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
