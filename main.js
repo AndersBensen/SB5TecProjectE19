eb9a9114a9dd1e516e36423a476c85957c6100ef
Erik <erik@hagebjerg.dk>
2019-10-17 13:28:01 +0200

    Edit structure and add client socket

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/server.html'));
+    res.redirect('client.html');
 });
 
 app.get('/new-server', (req, res) => {
--- wsServer.js
+++ wsServer.js
@@ -8,12 +8,12 @@ exports.newServer = function() {
     let id = 'asdf';
     let wsServer = new Server({port: 7000, path: '/' + id});
     servers.push(wsServer);
-    console.log('new server called');
-    console.log(servers[0]["options"]["path"]); 
+    // console.log(servers[0]["options"]["path"]);
+
+    wsServer.on('connection', ws => {
+        console.log(id);
+        ws.on('close', () => console.log('Closing connection..'));
+    });
+    
+    console.log('New server socket');
 }
-/* 
-wsServer.on('connection', ws => {
-    console.log(ws);
-    ws.on('close', () => console.log('Closing connection..'));
-});
- */
