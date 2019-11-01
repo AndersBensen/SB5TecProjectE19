446c2ce484873b86306ca3b8f6e0849d0f2c7a37
Anders Bensen Ottsen <anders_bensen@hotmail.com>
2019-11-01 08:20:43 +0100

    splitted the client up into two seperate pages with each their js logic

---
+++ client/gameClient.js
@@ -0,0 +1,92 @@
+let connected = false;
+let ws;
+let name;
+var canvas;
+
+let establishConnection = function() {
+  if (connected) return;
+
+    let url = document.getElementById("inputUrl").value
+    let port = document.getElementById("inputID").value;
+    console.log(port);
+    
+    ws = new WebSocket("ws://" + url + ":" + port);
+    ws.onopen = () => {
+        name = document.getElementById("inputName").value;
+        ws.send(JSON.stringify({ name }));
+        connected = true;
+        console.log("connected");
+        createCanvas();
+    };
+    ws.onclose = () => {
+        connected = false;
+        console.log("disconnected");
+    };
+    ws.onerror = e => console.log("Something went wrong:", e);
+
+    ws.onmessage = gameData => {
+        playerData = JSON.parse(gameData.data);
+        updateCanvas(playerData);
+
+        // console.log(playerData);
+    };
+};
+
+document.onkeydown = event => {
+  if (!connected) return;
+
+  switch (event.key) {
+    case "ArrowLeft":
+      ws.send(JSON.stringify({ name, keydown: "LEFT" }));
+      break;
+    case "ArrowRight":
+      ws.send(JSON.stringify({ name, keydown: "RIGHT" }));
+      break;
+    default:
+      console.log("Not a valid key");
+  }
+};
+
+document.onkeyup = event => {
+  if (!connected) return;
+
+  switch (event.key) {
+    case "ArrowLeft":
+      ws.send(JSON.stringify({ name, keyup: "LEFT" }));
+      break;
+    case "ArrowRight":
+      ws.send(JSON.stringify({ name, keyup: "RIGHT" }));
+      break;
+    default:
+      console.log("Not a valid key");
+  }
+};
+
+let createCanvas = () => {
+  canvas = document.createElement("canvas");
+  canvas.width = 800;
+  canvas.height = 350;
+  canvas.context = canvas.getContext("2d");
+  document.body.insertBefore(canvas, document.getElementById("empty"));
+
+  canvas.clear = () => {
+    canvas.context.clearRect(0, 0, canvas.width, canvas.height); // clear canvas in order to update current posisitons
+  };
+  console.log(canvas);
+};
+
+let updateCanvas = playerData => {
+  canvas.clear();
+
+  playerData.forEach(player => {
+    let ctx = canvas.context;
+    let points = player.positions;
+
+    console.log(player);
+    ctx.fillStyle = player.color;
+
+    points.forEach(point => {
+      ctx.fillRect(point.x, point.y, player.size, player.size);
+    });
+  });
+};
