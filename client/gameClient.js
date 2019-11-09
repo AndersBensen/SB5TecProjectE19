27265e91b3f30b06a3c8c5e688fbf8b5724f0ff8
Loc Hoang Thanh Nguyen <locnguyen@Locs-MacBook-Pro.local>
2019-11-09 11:36:06 +0100

    gui done

--- client/gameClient.js
+++ client/gameClient.js
@@ -4,44 +4,55 @@ let name;
 var canvas;
 
 let establishConnection = function() {
-    if (connected) return;
-    let port = document.getElementById("inputID").value;
-    getGame(port);
+  if (connected) return;
+  let port = document.getElementById("inputID").value;
+  getGame(port);
 };
 
 let initializeWebSocket = function(game) {
-    let url = document.getElementById("inputUrl").value
-    let port = document.getElementById("inputID").value;
-    amountOfPlayers = game['players'].length;
-    console.log(port);
-    console.log("player amount: " + amountOfPlayers);
+  let url = document.getElementById("inputUrl").value;
+  let port = document.getElementById("inputID").value;
+  amountOfPlayers = game["players"].length;
+  console.log(port);
+  console.log("player amount: " + amountOfPlayers);
 
-    if (amountOfPlayers > 3) {
-        document.getElementById("errmsg").innerHTML = "The max amount of players: " + amountOfPlayers + ", has been reached, join a new game or make your own.";
-    } else {
-        ws = new WebSocket("ws://" + url + ":" + port);
-        ws.onopen = () => {
-            name = document.getElementById("inputName").value;
-            ws.send(JSON.stringify({ name }));
-            connected = true;
-            console.log("connected");
-            createCanvas();
-        };
-        ws.onclose = () => {
-            connected = false;
-            console.log("disconnected");
-        };
-        ws.onerror = e => console.log("Something went wrong:", e);
+  if (amountOfPlayers > 3) {
+    document.getElementById("errmsg").innerHTML =
+      "The max amount of players: " +
+      amountOfPlayers +
+      ", has been reached, join a new game or make your own.";
+  } else {
+    ws = new WebSocket("ws://" + url + ":" + port);
+    ws.onopen = () => {
+      name = document.getElementById("inputName").value;
+      ws.send(JSON.stringify({ name }));
+      connected = true;
+      console.log("connected");
+      createCanvas();
+    };
+    ws.onclose = () => {
+      connected = false;
+      console.log("disconnected");
+    };
+    ws.onerror = e => console.log("Something went wrong:", e);
 
-        ws.onmessage = gameData => {
-            playerData = JSON.parse(gameData.data);
-            updateCanvas(playerData);
+    ws.onmessage = gameData => {
+      playerData = JSON.parse(gameData.data);
+      updateCanvas(playerData);
 
-            // console.log(playerData);
-        };
-    }
-}
+      let div = document.getElementById("opps");
+      let playerColor = document.getElementById("player-color");
+      div.innerHTML = "";
+      playerColor.innerHTML = "";
 
+      playerData.forEach(player => {
+        name = player.name;
+        color = player.color;
+        div.innerHTML += `<span style="color: ${color};"> ${name} </span> <br>`;
+      });
+    };
+  }
+};
 
 document.onkeydown = event => {
   if (!connected) return;
@@ -53,6 +64,8 @@ document.onkeydown = event => {
     case "ArrowRight":
       ws.send(JSON.stringify({ name, keydown: "RIGHT" }));
       break;
+    case " ":
+      ws.send(JSON.stringify({ name, keydown: "SPACE" }));
     default:
       console.log("Not a valid key");
   }
@@ -92,8 +105,6 @@ let updateCanvas = playerData => {
   playerData.forEach(player => {
     let ctx = canvas.context;
     let points = player.positions;
-
-    console.log(player);
     ctx.fillStyle = player.color;
 
     points.forEach(point => {
@@ -103,31 +114,31 @@ let updateCanvas = playerData => {
 };
 
 let getGame = function(port) {
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            let gameArr = [];
-            gameArr = JSON.parse(xmlHttp.responseText).playerArr;
-            if (gameArr !== undefined) {
-                initializeWebSocket(gameArr);
-            } 
-        }
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      let gameArr = [];
+      gameArr = JSON.parse(xmlHttp.responseText).playerArr;
+      if (gameArr !== undefined) {
+        initializeWebSocket(gameArr);
+      }
     }
-    xmlHttp.open("GET", "/get-game", true); // true for asynchronous 
-    xmlHttp.send(port);
-}
+  };
+  xmlHttp.open("GET", "/get-game", true); // true for asynchronous
+  xmlHttp.send(port);
+};
 
 let updateGameInfo = function(port) {
   let xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            let gameArr = [];
-            gameArr = JSON.parse(xmlHttp.responseText).playerArr;
-            if (gameArr !== undefined) {
-              //do something
-            } 
-        }
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      let gameArr = [];
+      gameArr = JSON.parse(xmlHttp.responseText).playerArr;
+      if (gameArr !== undefined) {
+        //do something
+      }
     }
-    xmlHttp.open("GET", "/get-game", true); // true for asynchronous 
-    xmlHttp.send(port);
-}
+  };
+  xmlHttp.open("GET", "/get-game", true); // true for asynchronous
+  xmlHttp.send(port);
+};
